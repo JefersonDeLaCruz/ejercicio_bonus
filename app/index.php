83618ce42ec30ba38a4bd116b70dd7b57c1350c4
JefersonDeLaCruz <dv23003@ues.edu.sv>
2025-09-25 11:54:55 -0600

    test

--- app/index.php
+++ app/index.php
@@ -1,5 +1,40 @@
-
-
 <?php
-    echo "avla";
+
+
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina principal</title>
+    <link rel="stylesheet" href="style.css">
+
+    <div class="box">
+        <div class="menu-nav">
+            <H4>Menu de navegacion</H4>
+            <ul>
+                <li><a href="./inicio.php">Inicio</a></li>
+                <li><a href="./lang.php">Mi lenguaje favorito</a></li>
+                <li><a href="./contacto.php">Contactar</a></li>
+            </ul>
+        </div>
+
+        <div>
+            <P>El objetivo de esta actividad es practicar lo vsto en clase y obtener un punto extra, vamo ahi</P>
+        </div>
+
+    </div>
+
+
+</head>
+
+<body>
+
+</body>
+
+</html>
